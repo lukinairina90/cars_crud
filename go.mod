223597fc3cde8a927ffc18322c45bc7db8ad7394
A.Krasheninnikova <o.krasheninnikova@andersenlab.com>
2022-02-15 15:20:18 +0200

    Created webCreate route

--- go.mod
+++ go.mod
@@ -3,13 +3,16 @@ module C_CRUD
 go 1.17
 
 require (
-	github.com/caarlos0/env/v6 v6.9.1 // indirect
-	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/caarlos0/env/v6 v6.9.1
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/sirupsen/logrus v1.8.1
+	gorm.io/driver/mysql v1.2.3
+	gorm.io/gorm v1.22.5
+)
+
+require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
 	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
-	gorm.io/driver/mysql v1.2.3 // indirect
-	gorm.io/gorm v1.22.5 // indirect
 )
--- server.go
+++ server.go
@@ -19,8 +19,13 @@ import (
 )
 
 type ViewData struct {
-	Title   string
-	Message string
+	gorm.Model
+	ID           int
+	ModelName    string
+	Type         string
+	Transmission string
+	Engine       string
+	HorsePower   string
 }
 
 type crud struct {
@@ -30,32 +35,6 @@ type crud struct {
 
 func main() {
 	// https://github.com/go-ozzo/ozzo-validation
-	// form method = POST , action = /api/cars
-	// form method = POST , action = /api/cars/{id}
-	//https://github.com/go-chi/chi
-	//https://www.newline.co/@kchan/building-a-simple-restful-api-with-go-and-chi--5912c411
-	// /api/cars/crudmethhods
-	// GET /api/cars index
-	// POST /api/cars store
-	// POST /api/cars/{id} update
-	// DELETE /api/cars/{id} delete (form method delete)
-	// /web/cars/index
-
-	// describe routes
-	// add gorm, describe car model, run migrations
-	// start POST /api/cars method
-
-	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	//	data := ViewData{
-	//		Title:   "Manage cars",
-	//		Message: "Cars",
-	//	}
-	//	tmpl, _ := template.ParseFiles("templates/index.html")
-	//	err := tmpl.Execute(w, data)
-	//	if err != nil {
-	//		return
-	//	}
-	//})
 
 	//ENV
 	cfg := Config{}
@@ -88,48 +67,25 @@ func main() {
 	}
 
 	r := chi.NewRouter()
-	// RESTy routes for "articles" resource
 	r.Route("/api", func(r chi.Router) {
 		r.Route("/cars", func(r chi.Router) {
-			r.Get("/", cr.listCars)          // GET /api/cars index
-			r.Put("/{id}", cr.updateCarById) // UPDATE /api/cars index
-			r.Post("/{id}", cr.listCarByID)  // POST /api/cars/{id} get
-			r.Post("/", cr.createCar)        // POST /api/cars/ create
-			r.Delete("/{id}", cr.deleteCar)  // /api/cars/{id} delete (form method delete)
+			r.Get("/", cr.listCars)
+			r.Post("/", cr.createCar)
+			r.Put("/{id}", cr.updateCarById)
+			r.Post("/{id}", cr.listCarByID)
+			r.Delete("/{id}", cr.deleteCar)
 		})
 	})
-
-	// RESTy routes for "articles" resource
 	r.Route("/cars", func(r chi.Router) {
-		r.Get("/", webIndex) // GET /api/cars index
+		r.Get("/", cr.webListCars)
+		r.Post("/", cr.webCreateCar)
+		r.Put("/{id}", cr.updateCarById)
+		r.Post("/{id}", cr.listCarByID)
+
 	})
 
-	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	//	data := ViewData{
-	//		Title:   "Manage cars",
-	//		Message: "Cars",
-	//	}
-	//	tmpl, _ := template.ParseFiles("templates/index.html")
-	//	err := tmpl.Execute(w, data)
-	//	if err != nil {
-	//		return
-	//	}
-	//})
-	//
-	//r := chi.NewRouter()
-	//r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-	//	_, err := w.Write([]byte("welcome"))
-	//	if err != nil {
-	//		return
-	//	}
-	//})
-	//// RESTy routes for "articles" resource
-	//r.Route("/articles", func(r chi.Router) {
-	//	r.Get("/", listCars) // GET /api/cars index
-	//	//r.Post("/{id}", listCarByID) // POST /api/cars/{id} edit
-	//	//r.Post("/", createCar)       // POST /api/cars/ create
-	//	//r.Delete("/", deleteCar)     // /api/cars/{id} delete (form method delete)
-	//
+	//r.Route("/cars", func(r chi.Router) {
+	//	r.Get("/", webIndex) // GET /api/cars index
 	//})
 
 	//Migration
@@ -143,63 +99,7 @@ func main() {
 	}
 }
 
-func (cr crud) deleteCar(w http.ResponseWriter, r *http.Request) {
-	key := chi.URLParam(r, "id")
-
-	logrus.WithField("car_id", key).Info("starting deletion car")
-
-	var car models.Car
-	//id, _ := strconv.ParseInt(key, 10, 64)
-	if err := cr.db.Debug().Where("id = ?", key).Delete(&car).Error; err != nil {
-		logrus.WithField("error", err).Error("error deleting car")
-	}
-
-	//cr.db.Delete(&car)
-	w.WriteHeader(http.StatusNoContent)
-}
-
-func (cr crud) listCarByID(w http.ResponseWriter, r *http.Request) {
-	key := chi.URLParam(r, "id")
-
-	var car models.Car
-	cr.db.First(&car, key)
-	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(car)
-	if err != nil {
-		return
-	}
-}
-
-func (cr crud) createCar(w http.ResponseWriter, r *http.Request) {
-	requestBody, _ := ioutil.ReadAll(r.Body)
-	var car models.Car
-	err := json.Unmarshal(requestBody, &car)
-	if err != nil {
-		return
-	}
-
-	cr.db.Create(&car)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	err = json.NewEncoder(w).Encode(&car)
-	if err != nil {
-		return
-	}
-}
-
-func webIndex(w http.ResponseWriter, r *http.Request) {
-	data := ViewData{
-		Title:   "Manage cars",
-		Message: "Cars",
-	}
-	tmpl, _ := template.ParseFiles("templates/index.html")
-	err := tmpl.Execute(w, data)
-	if err != nil {
-		return
-	}
-}
-
-func (cr crud) listCars(w http.ResponseWriter, r *http.Request) {
+func (cr crud) getCarsList() []Car {
 	var carsModels []models.Car
 	cr.db.Find(&carsModels)
 
@@ -217,9 +117,64 @@ func (cr crud) listCars(w http.ResponseWriter, r *http.Request) {
 			HorsePower:   carModel.HorsePower,
 		})
 	}
+	return carsResp
+}
 
+func (cr crud) webListCars(w http.ResponseWriter, r *http.Request) {
+	carsResp := cr.getCarsList()
+	tmpl, _ := template.ParseFiles("templates/index.html")
+	err := tmpl.Execute(w, carsResp)
+	if err != nil {
+		println(err)
+	}
+}
+
+func (cr crud) listCars(w http.ResponseWriter, r *http.Request) {
+	carsResp := cr.getCarsList()
 	w.Header().Set("Content-Type", "application/json")
 	err := json.NewEncoder(w).Encode(carsResp)
+	if err != nil {
+		return
+	}
+}
+
+func (cr crud) postCreateCar() models.Car {
+	var car models.Car
+	cr.db.Create(&car)
+	return car
+}
+
+func (cr crud) createCar(w http.ResponseWriter, r *http.Request) {
+	car := cr.postCreateCar()
+	requestBody, _ := ioutil.ReadAll(r.Body)
+	err := json.Unmarshal(requestBody, &car)
+	if err != nil {
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	err = json.NewEncoder(w).Encode(&car)
+	if err != nil {
+		return
+	}
+}
+
+func (cr crud) webCreateCar(w http.ResponseWriter, r *http.Request) {
+	carsResp := cr.postCreateCar()
+	tmpl, _ := template.ParseFiles("templates/index.html")
+	err := tmpl.Execute(w, carsResp)
+	if err != nil {
+		println(err)
+	}
+}
+
+func (cr crud) listCarByID(w http.ResponseWriter, r *http.Request) {
+	key := chi.URLParam(r, "id")
+
+	var car models.Car
+	cr.db.First(&car, key)
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(car)
 	if err != nil {
 		return
 	}
@@ -258,6 +213,17 @@ func (cr crud) updateCarById(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func createCars(writer http.ResponseWriter, request *http.Request) {
-	//request.Form.Get("sefe")
+func (cr crud) deleteCar(w http.ResponseWriter, r *http.Request) {
+	key := chi.URLParam(r, "id")
+
+	logrus.WithField("car_id", key).Info("starting deletion car")
+
+	var car models.Car
+	//id, _ := strconv.ParseInt(key, 10, 64)
+	if err := cr.db.Debug().Where("id = ?", key).Delete(&car).Error; err != nil {
+		logrus.WithField("error", err).Error("error deleting car")
+	}
+
+	//cr.db.Delete(&car)
+	w.WriteHeader(http.StatusNoContent)
 }
